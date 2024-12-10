4bd105f90747c6e1bf9a1f2fa3dc101b9f55d03f
Melati M. Roring <22210047@unima.ac.id>
2024-12-10 23:44:03 +0800

    menghapus duplikat output

--- infrastructure/terraform/modules/vpc/outputs.tf
+++ infrastructure/terraform/modules/vpc/outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
 output "public_subnets" {
   description = "IDs of the public subnets"
   value       = aws_subnet.public[*].id
